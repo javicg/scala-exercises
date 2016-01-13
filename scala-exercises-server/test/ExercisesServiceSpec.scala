e6ff2626879dd006dd56f020888f2e6cd17dad16
Rafa Paradela <rafa.p@47deg.com>
2016-01-13 16:38:07 +0100

    Changed ExercisesServiceSpec after renaming library/section/category convention

--- scala-exercises-server/test/ExercisesServiceSpec.scala
+++ scala-exercises-server/test/ExercisesServiceSpec.scala
@@ -3,51 +3,51 @@ import org.junit.runner._
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.specs2.scalaz.DisjunctionMatchers
-import services.parser.ExercisesService
+import services.exerciseV0.ExercisesService
 
 @RunWith(classOf[JUnitRunner])
 class ExercisesServiceSpec extends Specification with DisjunctionMatchers {
 
-  val expectedTestSection = "stdlib"
-  val expectedTestCategory = "Extractors"
+  val expectedTestLibrary = "stdlib"
+  val expectedTestSection = "Extractors"
   val expectedTestExercise = "forAssigningValues"
   val expectedTestSuccesArgs = List("Chevy", "Camaro", "1978", "120")
   val expectedTestFailedArgs = List("a", "b", "1", "2")
 
   "ExercisesService" should {
 
-    "return at least one section via classpath discovery" in {
-      val sections = ExercisesService.sections
-      sections must not be empty
-      sections.find(_.title == expectedTestSection) must beSome
+    "return at least one library via classpath discovery" in {
+      val libraries = ExercisesService.libraries
+      libraries must not be empty
+      libraries.find(_.name == expectedTestLibrary) must beSome
     }
 
-    "return at least one category via classpath discovery" in {
-      val foundCategories = for {
-        section ← ExercisesService.sections
-        categoryS ← section.categories
-        category ← ExercisesService.category(section.title, categoryS)
-      } yield category
-      foundCategories must not be empty
-      val expectedCat = foundCategories.find(_.title == expectedTestCategory)
-      expectedCat must beSome
-      val category = expectedCat.get
-      category.exercises must not be empty
-      category.exercises.find(_.method.contains(expectedTestExercise)) must beSome
+    "return at least one section via classpath discovery" in {
+      val foundSections = for {
+        library ← ExercisesService.libraries
+        sectionName ← library.sectionNames
+        section ← ExercisesService.section(library.name, sectionName)
+      } yield section
+      foundSections must not be empty
+      val expectedSection = foundSections.find(_.name == expectedTestSection)
+      expectedSection must beSome
+      val section = expectedSection.get
+      section.exercises must not be empty
+      section.exercises.find(_.method.contains(expectedTestExercise)) must beSome
     }
 
     "evaluate a known exercise type coercing it's parameters and get a successful result" in {
       ExercisesService.evaluate(ExerciseEvaluation(
-        section = expectedTestSection,
-        category = expectedTestCategory,
+        libraryName = expectedTestLibrary,
+        sectionName = expectedTestSection,
         method = expectedTestExercise,
         args = expectedTestSuccesArgs)) must beRightDisjunction
     }
 
     "evaluate a known exercise type coercing it's parameters and get a failed result" in {
       ExercisesService.evaluate(ExerciseEvaluation(
-        section = expectedTestSection,
-        category = expectedTestCategory,
+        libraryName = expectedTestLibrary,
+        sectionName = expectedTestSection,
         method = expectedTestExercise,
         args = expectedTestFailedArgs)) must beLeftDisjunction
     }
